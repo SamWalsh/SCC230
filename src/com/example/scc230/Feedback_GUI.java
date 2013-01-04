b3f2419aa26119f3d6bb02a02ffb4d27bc6a47bc
SamWalsh <samuel.d.walsh@gmail.com>
2013-01-04 21:16:50 +0000

    initial commit

---
+++ src/com/example/scc230/Feedback_GUI.java
@@ -0,0 +1,22 @@
+package com.example.scc230;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Feedback_GUI extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_feedback__gui);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_feedback__gui, menu);
+		return true;
+	}
+
+}
