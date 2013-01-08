f5b4fda3a8a7d8f75136d3028148faf6f7db0dc0
SamWalsh <samuel.d.walsh@gmail.com>
2013-01-08 20:08:09 +0000

    Big add. Ant's + Ryan's + Josh's + Sam's added
    New images, all now .png - everything merged together, Log in,
    timetable, feedback GUI, notifications, lecturer comments GUI

--- gen/com/example/scc230/R.java
+++ gen/com/example/scc230/R.java
@@ -11,60 +11,84 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int back1=0x7f020000;
-        public static final int cam=0x7f020001;
+        public static final int back=0x7f020000;
+        public static final int bigredandgrey=0x7f020001;
         public static final int comment=0x7f020002;
-        public static final int crg=0x7f020003;
-        public static final int gbb=0x7f020004;
-        public static final int greyandred=0x7f020005;
-        public static final int greybox=0x7f020006;
-        public static final int greybox1=0x7f020007;
-        public static final int homebutton=0x7f020008;
-        public static final int ic_launcher=0x7f020009;
-        public static final int notifications1=0x7f02000a;
-        public static final int rb=0x7f02000b;
-        public static final int settings=0x7f02000c;
-        public static final int title=0x7f02000d;
-        public static final int title2=0x7f02000e;
-        public static final int wb=0x7f02000f;
-        public static final int whitebox=0x7f020010;
+        public static final int home=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int notifications=0x7f020005;
+        public static final int picture=0x7f020006;
+        public static final int redandgrey=0x7f020007;
+        public static final int spanner=0x7f020008;
+        public static final int thingreybox=0x7f020009;
+        public static final int topbar=0x7f02000a;
+        public static final int white=0x7f02000b;
     }
     public static final class id {
-        public static final int CheckBox01=0x7f070012;
-        public static final int ImageView01=0x7f07000c;
-        public static final int TextView01=0x7f07000b;
-        public static final int TextView02=0x7f07000d;
-        public static final int TextView03=0x7f070013;
-        public static final int TextView04=0x7f070014;
-        public static final int checkBox1=0x7f07000f;
-        public static final int editText1=0x7f070011;
+        public static final int CheckBox01=0x7f070011;
+        public static final int EditText01=0x7f070018;
+        public static final int ImageView01=0x7f07000b;
+        public static final int ImageView02=0x7f070021;
+        public static final int TextView01=0x7f07000a;
+        public static final int TextView02=0x7f07000c;
+        public static final int TextView03=0x7f070012;
+        public static final int TextView04=0x7f070013;
+        public static final int button1=0x7f070019;
+        public static final int checkBox1=0x7f07000e;
+        public static final int editText1=0x7f070010;
+        public static final int editText10=0x7f070023;
+        public static final int editText11=0x7f070024;
+        public static final int editText12=0x7f070025;
+        public static final int editText2=0x7f070017;
+        public static final int editText3=0x7f07001c;
+        public static final int editText4=0x7f07001d;
+        public static final int editText5=0x7f070026;
+        public static final int editText6=0x7f07001f;
+        public static final int editText7=0x7f07001e;
+        public static final int editText8=0x7f070020;
+        public static final int editText9=0x7f070022;
         public static final int imageButton1=0x7f070002;
         public static final int imageButton2=0x7f070003;
         public static final int imageButton3=0x7f070004;
-        public static final int imageButton4=0x7f070005;
+        public static final int imageButton4=0x7f070014;
         public static final int imageView1=0x7f070001;
-        public static final int imageView2=0x7f070006;
-        public static final int imageView3=0x7f070008;
-        public static final int imageView4=0x7f070009;
-        public static final int imageView5=0x7f070010;
+        public static final int imageView2=0x7f070005;
+        public static final int imageView3=0x7f070007;
+        public static final int imageView4=0x7f070008;
+        public static final int imageView5=0x7f07000f;
         public static final int imageView6=0x7f070015;
-        public static final int menu_settings=0x7f070016;
-        public static final int ratingBar1=0x7f07000e;
+        public static final int imageView7=0x7f07001a;
+        public static final int menu_settings=0x7f070027;
+        public static final int ratingBar1=0x7f07000d;
         public static final int scroll=0x7f070000;
-        public static final int textView1=0x7f070007;
-        public static final int textView2=0x7f07000a;
+        public static final int textView1=0x7f070006;
+        public static final int textView2=0x7f070009;
+        public static final int textView3=0x7f070016;
+        public static final int textView4=0x7f07001b;
     }
     public static final class layout {
         public static final int activity_feedback__gui=0x7f030000;
+        public static final int activity_lecturer__reply__gui=0x7f030001;
+        public static final int activity_login_screen=0x7f030002;
+        public static final int activity_notifications__gui=0x7f030003;
+        public static final int activity_timetable__gui=0x7f030004;
     }
     public static final class menu {
         public static final int activity_feedback__gui=0x7f060000;
+        public static final int activity_lecturer__reply__gui=0x7f060001;
+        public static final int activity_login_screen=0x7f060002;
+        public static final int activity_notifications__gui=0x7f060003;
+        public static final int activity_timetable__gui=0x7f060004;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int home=0x7f040004;
         public static final int menu_settings=0x7f040002;
+        public static final int title_activity_lecturer__reply__gui=0x7f040007;
+        public static final int title_activity_login_screen=0x7f040006;
+        public static final int title_activity_notifications__gui=0x7f040008;
+        public static final int title_activity_timetable__gui=0x7f040005;
         public static final int top_title=0x7f040003;
     }
     public static final class style {
